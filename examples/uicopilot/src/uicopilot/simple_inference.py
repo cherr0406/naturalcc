da29a56c40301f0504d86b4186728ffeeb8a069e
cherr0406 <82948891+cherr0406@users.noreply.github.com>
2025-05-25 22:24:10 +0900

    chore: Move simple_inference.py to src/uicopilot/ and prepare for merging upstream changes

--- examples/uicopilot/src/uicopilot/simple_inference.py
+++ examples/uicopilot/src/uicopilot/simple_inference.py
@@ -1,156 +1,115 @@
-import logging
-from typing import Any
-
+import os
 import torch
 from PIL import Image
-from transformers.models.pix2struct import Pix2StructForConditionalGeneration, Pix2StructProcessor
-from transformers.models.t5 import T5Tokenizer, T5TokenizerFast
+from scripts.train.utils import Html2BboxTree, move_to_device, BboxTree2Html, add_special_tokens, Html2BboxTree, BboxTree2StyleList, BboxTree2Html
+from transformers import AutoProcessor, Pix2StructForConditionalGeneration
+from agents.utils.tools import *
+from agents import *
 
-from .agents import AgentI2C, AgentOptimize
-from .utils import BboxTree2Html, BboxTree2StyleList, Html2BboxTree, add_special_tokens, move_to_device
+device = 'cuda'
 
-logger = logging.getLogger(__name__)
+processor = AutoProcessor.from_pretrained("anonymouscodee/webcoder")
+model_bbox = Pix2StructForConditionalGeneration.from_pretrained("xcodemind/uicopilot_structure ", is_encoder_decoder=True, device_map=device, torch_dtype=torch.float16)
+add_special_tokens(model_bbox,processor.tokenizer)
 
+agent_i2c = AgentI2C()
+agent_optimize = AgentOptimize()
 
-class SimpleInference:
-    def __init__(self, agent_i2c: AgentI2C, agent_optimize: AgentOptimize):
-        pretrained_model_name: str = "xcodemind/webcoder"
-        self.device: str = "cuda" if torch.cuda.is_available() else "cpu"
+def infer_bbox(image):
+    model_bbox.eval()
+    with torch.no_grad():
+        input = f'<body bbox=['
+        decoder_input_ids = processor.tokenizer.encode(input,return_tensors='pt',add_special_tokens=True)[...,:-1]
+        encoding = processor(images=[image],text=[""],max_patches=1024,return_tensors='pt')
+        item = {
+            'decoder_input_ids': decoder_input_ids,
+            'flattened_patches': encoding['flattened_patches'].half(),
+            'attention_mask': encoding['attention_mask']
+        }
+        item = move_to_device(item,device)
+    
+        outputs = model_bbox.generate(**item,max_new_tokens=2560,eos_token_id=processor.tokenizer.eos_token_id,do_sample=True)
+                
+        prediction_html = processor.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
 
-        processor_or_tuple = Pix2StructProcessor.from_pretrained(pretrained_model_name)
-        self.processor = processor_or_tuple[0] if isinstance(processor_or_tuple, tuple) else processor_or_tuple
-        self.tokenizer: T5Tokenizer | T5TokenizerFast = self.processor.tokenizer  # type: ignore
+    return prediction_html
 
-        self.model_bbox = Pix2StructForConditionalGeneration.from_pretrained(
-            pretrained_model_name,
-            is_encoder_decoder=True,
-            device_map=self.device,
-            torch_dtype=torch.float16 if self.device == "cuda" else torch.float32,
-        )
-        add_special_tokens(self.model_bbox, self.tokenizer)
+def locateByIndex(bboxTree,index):
+    target = bboxTree
+    for i in list(filter(lambda x: x,index.split('-'))):
+        target = target['children'][int(i)]
+    return target
 
-        self.agent_i2c = agent_i2c
-        self.agent_optimize = agent_optimize
+def extract_html(html):
+    if '```' in html:
+        html = html.split('```')[1]
+    if html[:4] == 'html':
+        html = html[4:]
+    html = html.strip()
+    return html
 
-    def infer_bbox(self, image: Any) -> str:
-        self.model_bbox.eval()
-        with torch.no_grad():
-            _input = "<body bbox=["
-            decoder_input_ids = torch.Tensor(self.tokenizer.encode(_input, return_tensors="pt", add_special_tokens=True))
-            decoder_input_ids = decoder_input_ids[..., :-1]
-            encoding = self.processor(images=[image], text=[""], return_tensors="pt", images_kwargs={"max_patches": 1024})
-            item = {
-                "decoder_input_ids": decoder_input_ids,
-                "flattened_patches": encoding["flattened_patches"].half() if self.device == "cuda" else encoding["flattened_patches"],  # type: ignore
-                "attention_mask": encoding["attention_mask"],
-            }
-            item = move_to_device(item, self.device)
+def pruning(node, now_depth, max_depth, min_area):
+    bbox = node['bbox']
+    area = abs((bbox[2]-bbox[0])*(bbox[3]-bbox[1]))
+    if area < min_area:
+        return None
+    if now_depth >= max_depth:
+        node['children'] = []
+    else:
+        for idx, cnode in enumerate(node['children']):
+            node['children'][idx] = pruning(cnode, now_depth+1, max_depth, min_area)
+        node['children'] = list(filter(lambda x:x, node['children']))
+    return node
 
-            outputs = self.model_bbox.generate(**item, max_new_tokens=2560, eos_token_id=self.tokenizer.eos_token_id, do_sample=True)
+def gen(image, max_depth=100, min_area=100):
+    imgs = []
+    # infer
+    prediction_html = infer_bbox(image)
 
-            prediction_html = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+    # draw bbox on image
+    pBbox = Html2BboxTree(prediction_html, size=image.size)
 
-        return prediction_html
+    # pruning
+    pruning(pBbox, 1, max_depth, min_area)
 
-    def locateByIndex(self, bboxTree: dict[str, Any], index: str) -> dict:
-        target = bboxTree
-        for i in list(filter(lambda x: x, index.split("-"))):
-            target = target["children"][int(i)]
-        return target
+    # iter leaf node to gen ctree by agent
+    bboxTree = Html2BboxTree(prediction_html, size=image.size)
+    indexList = BboxTree2StyleList(bboxTree, skip_leaf=False)
+    # only leaf filter
+    indexList = list(filter(lambda x:not len(x['children']), indexList))
+    img_count = 0
+    for item in indexList:
+        bbox = item['bbox']
+        index_ = item['index']
 
-    def extract_html(self, html: str) -> str:
-        if "```" in html:
-            html = html.split("```")[1]
-        if html[:4] == "html":
-            html = html[4:]
-        html = html.strip()
-        return html
-
-    def pruning(self, node: dict[str, Any], now_depth: int, max_depth: int, min_area: int) -> dict[str, Any] | None:
-        bbox: list = node["bbox"]
-        area: int = abs((bbox[2] - bbox[0]) * (bbox[3] - bbox[1]))
-        if area < min_area:
-            return None
-        if now_depth >= max_depth:
-            node["children"] = []
+        image_crop = image.crop((bbox[0],bbox[1],bbox[0]+bbox[2],bbox[1]+bbox[3]))
+                
+        if item['type'] == 'img':
+            imgs.append(image_crop)
+            new_src = f'{img_count}.png'
+            part_html = new_src
+            img_count += 1
         else:
-            for idx, cnode in enumerate(node["children"]):
-                node["children"][idx] = self.pruning(cnode, now_depth + 1, max_depth, min_area)
-            node["children"] = list(filter(lambda x: x, node["children"]))
-        return node
+            part_html = agent_i2c.infer(image_crop)
+            part_html = extract_html(part_html)
 
-    def gen(self, image: Image.Image, max_depth: int = 100, min_area: int = 100) -> tuple[str, str, list[Image.Image]]:
-        imgs: list[Image.Image] = []
-        # infer
-        prediction_html = self.infer_bbox(image)
+        target = locateByIndex(bboxTree, index_)
+        target['children'] = [part_html]
 
-        # pruning
-        bboxTree = Html2BboxTree(prediction_html, size=image.size)
-        if bboxTree is None:
-            logger.error("bboxTree is None. Returning empty string, empty list")
-            return "", "", []
-        bboxTree = self.pruning(bboxTree, 1, max_depth, min_area)
-        if bboxTree is None:
-            logger.error("bboxTree is None after pruning. Returning empty string, empty list")
-            return "", "", []
+    html = BboxTree2Html(bboxTree, style=True)
 
-        # iter leaf node to gen ctree by agent
-        indexList: list = BboxTree2StyleList(bboxTree, skip_leaf=False)
-        # only leaf filter
-        indexList = list(filter(lambda x: not len(x["children"]), indexList))
-        img_count: int = 0
-        for item in indexList:
-            bbox: list = item["bbox"]
-            index_: str = item["index"]
+    # optmize by agent
+    html2 = agent_optimize.infer(image, html)
+    html2 = extract_html(html2)
+    return html, html2, imgs
 
-            image_crop: Any = image.crop((bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]))
+def __main__():
+    output_dir = './output'
+    image = Image.open('test.png')
 
-            if item["type"] == "img":
-                imgs.append(image_crop)
-                new_src: str = f"{img_count}.png"
-                part_html: str = new_src
-                img_count += 1
-            else:
-                part_html = self.agent_i2c.infer(image_crop)
-                part_html = self.extract_html(part_html)
-
-            target: dict = self.locateByIndex(bboxTree, index_)
-            assert target is not None
-            target["children"] = [part_html]
-
-        html: str = BboxTree2Html(bboxTree, style=True)
-
-        # optmize by agent
-        html2: str = self.agent_optimize.infer(image, html)
-        html2 = self.extract_html(html2)
-        return html, html2, imgs
-
-
-if __name__ == "__main__":
-    import os
-
-    logging.basicConfig(level=logging.DEBUG)
-
-    # Set environment variables
-    API_KEY = os.getenv("API_KEY") or ""
-    ENDPOINT = os.getenv("ENDPOINT") or ""
-    API_KEY_GEMINI = os.getenv("API_KEY_GEMINI") or ""
-    API_KEY_CLAUDE = os.getenv("API_KEY_CLAUDE") or ""
-
-    os.environ["HTTP_PROXY"] = "http://127.0.0.1:27890"
-    os.environ["HTTPS_PROXY"] = "http://127.0.0.1:27890"
-
-    # test
-    output_dir = "./output"
-    image = Image.open("test.png")
-
-    inference = SimpleInference(
-        AgentI2C(llm_model="gpt-4o", api_key=API_KEY, endpoint=ENDPOINT),
-        AgentOptimize(llm_model="gpt-4o", api_key=API_KEY, endpoint=ENDPOINT),
-    )
-    _, html, imgs = inference.gen(image)
+    _, html, imgs = gen(image)
     os.makedirs(output_dir, exist_ok=True)
-    with open(f"{output_dir}/index.html", "w") as f:
+    with open(f'{output_dir}/index.html', 'w') as f:
         f.write(html)
     for idx, img in enumerate(imgs):
-        img.save(f"{idx}.png")
+        img.save(f'{idx}.png')
