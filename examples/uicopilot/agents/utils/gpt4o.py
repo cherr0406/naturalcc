80ace24ee0e9b60a2274ddea6a047f1c84cad9ef
cherr0406 <82948891+cherr0406@users.noreply.github.com>
2025-05-26 12:15:29 +0000

    fix: update gpt4o to use environment variables

--- examples/uicopilot/agents/utils/gpt4o.py
+++ examples/uicopilot/agents/utils/gpt4o.py
@@ -2,9 +2,16 @@ import cv2
 import io
 import base64
 import requests
-from .config import *
+import os
+# from .config import *
 
 def gpt4o(prompt, image, text):
+    API_KEY = os.getenv("AZURE_OPENAI_API_KEY", "")
+    endpoint_base = os.getenv("AZURE_OPENAI_ENDPOINT", "")
+    deployment_id = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME", "")
+    api_version = os.getenv("AZURE_OPENAI_API_VERSION", "2024-05-13")
+    ENDPOINT= f"https://{endpoint_base}/openai/deployments/{deployment_id}/chat/completions?api-version={api_version}"
+    
     buffered = io.BytesIO()
     image.save(buffered, format="PNG")
     img_str = buffered.getvalue()
